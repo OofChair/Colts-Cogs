7fc91a737a09324b1a1c83c3115bbce118362d24
PredaMart <predaamart@outlook.com>
2020-04-23 01:51:14 +0200

    Several fixes, and output changes.

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -1,65 +1,52 @@
-from redbot.core import commands
-from redbot.core import checks
-from redbot.core import Config
 import discord
-import asyncio
+from redbot.core.bot import Red
+from redbot.core import commands, checks
+from redbot.core.utils.chat_formatting import box
+from redbot.core.utils.common_filters import URL_RE
+
+from datetime import datetime
 import re
-import os
 import subprocess
-import datetime
 
-try:
-    import speedtest
-    module_avail = True
-except ImportError:
-    module_avail = False
+DOWNLOAD_RE = re.compile(r"Download: ([\d.]+) .bit")
+UPLOAD_RE = re.compile(r"Upload: ([\d.]+) .bit")
+PING_RE = re.compile(r"([\d.]+) ms")
 
-BaseCog = getattr(commands, "Cog", object)
 
-class Speedtest(BaseCog):
+class Speedtest(commands.Cog):
     """Speedtest for your bot's server"""
 
-    def __init__(self, bot):
+    def __init__(self, bot: Red):
         self.bot = bot
-        self.config = Config.get_conf(self, 24341100993)
-        self.direct = "data/speedtest/settings.json"
-        self.users = {}
-        self.messages = {}
 
-    def speed_test(self):
-        return str(subprocess.check_output(['speedtest-cli'], stderr=subprocess.STDOUT))
+    def _speedtest(self):
+        with subprocess.Popen(
+            ["speedtest-cli --share"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+        ) as resp:
+            return str(resp.communicate())
 
-    @commands.command(no_pm=False)
     @checks.is_owner()
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
     async def speedtest(self, ctx):
+        """Runs a speedtest and prints the result."""
         try:
-            channel = ctx.message.channel
-            author = ctx.message.author
-            user = author
-            now = datetime.datetime.now()
-            message12 = await ctx.send(" :stopwatch: **Running speedtest. This may take a while!** :stopwatch:")
-            DOWNLOAD_RE = re.compile(r"Download: ([\d.]+) .bit")
-            UPLOAD_RE = re.compile(r"Upload: ([\d.]+) .bit")
-            PING_RE = re.compile(r"([\d.]+) ms")
-            speedtest_result = await self.bot.loop.run_in_executor(None, self.speed_test)
+            em = discord.Embed(
+                color=await ctx.embed_colour(),
+                title="⏱ Running speedtest ... This may take a while! ⏱",
+            )
+            msg = await ctx.send(embed=em)
+            speedtest_result = await self.bot.loop.run_in_executor(None, self._speedtest)
             download = float(DOWNLOAD_RE.search(speedtest_result).group(1))
             upload = float(UPLOAD_RE.search(speedtest_result).group(1))
             ping = float(PING_RE.search(speedtest_result).group(1))
-            message = 'Your speedtest results are:'
-            message_down = '**{}** mbps'.format(download)
-            message_up = '**{}** mbps'.format(upload)
-            message_ping = '**{}** ms'.format(ping)
-            colour = 0x45FF00
-            embed = discord.Embed(colour=colour, description=message)
-            embed.title = 'Speedtest Results'
-            embed.add_field(name='Download', value=message_down)
-            embed.add_field(name=' Upload', value=message_up)
-            embed.add_field(name=' Ping', value=message_ping)
-            embed.set_footer(text= now.strftime("%Y-%m-%d %H:%M"))
-            await ctx.send(embed=embed)
-        except KeyError:
-            print("An error occured")
-
-    if module_avail == False:
-        ctx.send("You currently don't have speedtest-cli installed, you need to run `pip install speedtest-cli`");
-        raise RuntimeError("You need to run `pip3 install speedtest-cli`")
+            img = str(URL_RE.search(speedtest_result).group(1))
+            embed = discord.Embed(color=0x10A714, title="Your speedtest results are:")
+            embed.add_field(name="Download", value=box(f"{download} mbps", lang="py"))
+            embed.add_field(name="Upload", value=box(f"{upload} mbps", lang="py"))
+            embed.add_field(name="Ping", value=box(f"{ping} ms", lang="py"))
+            embed.set_image(url=img)
+            embed.set_footer(text=datetime.now().strftime("Server time: %d-%m-%Y  %H:%M:%S"))
+            await msg.edit(embed=embed)
+        except KeyError as error:
+            await ctx.send(f"An error occured.\n{error}")

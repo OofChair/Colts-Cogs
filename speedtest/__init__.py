8fea842d949a0f9f0696ce0bb54b6dbd2dbc58e2
PredaMart <predaamart@outlook.com>
2020-04-23 02:56:27 +0200

    Prevent speedtest-cli not installed.

--- speedtest/__init__.py
+++ speedtest/__init__.py
@@ -2,5 +2,9 @@ from .speedtest import Speedtest
 
 
 def setup(bot):
+    try:
+        import speedtest
+    except ImportError:
+        raise ImportError("Failed to import speedtest-cli. Please do pip install speedtest-cli.")
     cog = Speedtest(bot)
     bot.add_cog(cog)
